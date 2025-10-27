518ae3ffe890ad7bd5692522a9d91156198ceb1c
pablo9pablo <127230216+pablo9pablo@users.noreply.github.com>
2025-10-27 17:22:26 +0200

    Add function to get computer choice in game

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Hello World")
+function getComputerchoice(){
+  let resultado="";
+  let valor=Math.random();
+  if(valor<=0.33){
+    resultado="piedra";
+  }else if(valor>0.33 && valor<=0.66){
+    resultado="papel";
+  }else{
+    resultado="tijera";
+  }
+  return resultado;
+}
+console.log(getComputerChoice());
